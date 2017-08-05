9a4ac8f131183f4a024ca01be0e34ed77ee73574
imran1234567 <imran@kgec.edu.in>
2017-08-05 21:34:09 +0530

    gg

--- pmt/src/tweets/apps.py
+++ pmt/src/tweets/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 
 
 class TweetsConfig(AppConfig):
-    name = 'tweets'
+    name = 'pmt.tweets'
--- pmt/src/tweets/views.py
+++ pmt/src/tweets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Tweet
 from django.views.generic import DetailView, ListView
 
@@ -6,11 +6,17 @@ from django.views.generic import DetailView, ListView
 
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
+    #template_name = "tweets/detail_view.html"
 
-    def get_object(self):
-        return Tweet.objects.get(id=1)
+
+    # def get_object(self):
+    #     print (self.kwargs)
+    #     pk = self.kwargs.get("pk")
+    #     print (pk)
+    #     return Tweet.objects.get(id=pk)
 
 class TweetListView(ListView):
+    #template_name = "tweets/list_view.html"
     queryset = Tweet.objects.all()
 
     def get_context_data(self, *args, **kwargs):
@@ -18,13 +24,14 @@ class TweetListView(ListView):
         print (context)
         return context
 
-# def tweet_detail_view(request,id=1):
-#     obj = Tweet.objects.get(id=id)
-#     print (obj)
-#     context = {
-#         "object":obj
-#     }
-#     return render(request, "tweets/detail_view.html", context)
+def tweet_detail_view(request,pk=None):
+    #obj = Tweet.objects.get(pk=pk)
+    obj = get_object_or_404(Tweet, pk=pk)
+    print (obj)
+    context = {
+        "object":obj
+    }
+    return render(request, "tweets/detail_view.html", context)
 
 
 # def tweet_list_view(request):
