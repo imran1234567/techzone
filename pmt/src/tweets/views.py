b7e8b2f15774dc84e4673789d9bc0a1c2d83aff1
imran1234567 <imran@kgec.edu.in>
2017-08-09 01:05:38 +0530

    gg

--- pmt/src/tweets/views.py
+++ pmt/src/tweets/views.py
@@ -1,16 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import Tweet
-from django.urls import reverse_lazy , reverse
+from django.urls import  reverse_lazy
 from .forms import TweetModelForm
 from .mixins import FormUserNeededMixin, UserOwnerMixin
-from django.views.generic import (
-                DetailView,
-                ListView,
-                CreateView,
-                UpdateView,
-                DeleteView
-                )
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+
 
 # Create your views here.
 
@@ -27,13 +23,13 @@ class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
     queryset = Tweet.objects.all()
     form_class = TweetModelForm
     template_name = 'tweets/update_view.html'
-    success_url = "/tweet/"
+    #success_url = "/tweet/"
 
 
 class TweetDeleteView(LoginRequiredMixin, DeleteView):
     model = Tweet
     template_name = 'tweets/delete_confirm.html'
-    success_url = reverse("tweet:list")
+    success_url = reverse_lazy("tweet:list")
 
 
 #Retrive
@@ -49,8 +45,17 @@ class TweetDetailView(DetailView):
     #     return Tweet.objects.get(id=pk)
 
 class TweetListView(ListView):
-    #template_name = "tweets/list_view.html"
-    queryset = Tweet.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print (self.request.GET)
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+            )
+        return qs
 
     def get_context_data(self, *args, **kwargs):
         context = super(TweetListView,self).get_context_data(*args, **kwargs)
